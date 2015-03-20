4af4188a2d19df7a2102cf0b3d1039c002da7b5b
SAM HAINS <sdh@eml.cc>
2015-03-20 11:45:38 -0400

    added mongoose backend

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var models = require('./models');
+var Message = models.Message;
+var mongoose = require('mongoose');
+var swig = require('swig');
+
+//set up swig as render engine
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  var messages = Message.find(function(err,messages){
+  	 //console.log(messages);
+     res.render('index',{messages:messages});
+  });
+
+
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  	var message = new Message({ body: msg });
+  	message.save(function(err){
+  		if(err) console.log(err);
+  		else
+			io.emit('chat message', msg);
+  	});
+    
   });
 });
 
